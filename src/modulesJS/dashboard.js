a66541910a59001211baf439cdc0c30884bdf8b5
Eduardo Neves <eduardo.neves@oliveiratrust.com.br>
2024-04-19 17:22:28 -0300

    feat: iniciando menu

--- src/modulesJS/dashboard.js
+++ src/modulesJS/dashboard.js
@@ -1,12 +1,15 @@
 import { toggleClasse } from '@/utils';
 
 const sidebarElement = document.querySelector('.dashboard-layout-sidebar');
+const contentElement = document.querySelector('.dashboard-layout-content');
 const togglerElement = document.querySelector('.sidebar-toggler');
+
 
 if (sidebarElement && togglerElement) {
 	togglerElement.addEventListener('click', (event) => {
 		event.preventDefault();
 
 		toggleClasse(sidebarElement, 'opened');
+		toggleClasse(contentElement, 'opened');
 	});
 }
