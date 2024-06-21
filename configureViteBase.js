af8898ce337341ce1fe6d9b3a14efeee23d9b313
Eduardo Neves <eduardo.neves@oliveiratrust.com.br>
2024-06-21 20:55:15 -0300

    feat: adicionando actions github pages 11

---
+++ configureViteBase.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+const repoName = process.env.GITHUB_REPOSITORY.split('/')[1];
+const viteConfigPath = 'vite.config.js';
+
+// Leitura do conteúdo do arquivo vite.config.js
+fs.readFile(viteConfigPath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Erro ao ler o arquivo vite.config.js:', err);
+    process.exit(1);
+  }
+
+  // Substituição da linha 'base: ""' pela configuração apropriada
+  const result = data.replace(/base: ""/, `base: "/${repoName}/"`);
+
+  // Escrita do conteúdo atualizado no arquivo vite.config.js
+  fs.writeFile(viteConfigPath, result, 'utf8', (err) => {
+    if (err) {
+      console.error('Erro ao escrever no arquivo vite.config.js:', err);
+      process.exit(1);
+    }
+    console.log('A configuração base foi adicionada ao arquivo vite.config.js.');
+  });
+});
