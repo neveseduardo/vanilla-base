152ea91a31f9e154de9cf0668f5bd1856e7fafa1
Eduardo Neves <eduardo.neves@oliveiratrust.com.br>
2024-06-21 20:35:06 -0300

    feat: adicionando actions github pages 3

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,18 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [],
-	assetsInclude: ['**/*.html'],
-	server: {
-		port: 8080,
-		host: true,
-		open: true,
-		fs: {
-			strict: false,
-		},
-	},
-	resolve: {
-		alias: { '@': resolve(__dirname, './src') },
-	},
+  plugins: [],
+  assetsInclude: ['**/*.html'],
+  server: {
+    port: 8080,
+    host: true,
+    open: true,
+    fs: {
+      strict: false,
+    },
+  },
+  resolve: {
+    alias: { '@': resolve(__dirname, './src') },
+  },
+  base: '', // Espaço reservado para a base
 });
